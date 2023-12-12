10cea7bc3c9da2d5b549b40bc909f11edbaeb8a9
Pickersgill <jack.pp@btopenworld.com>
2023-12-12 23:09:45 +0000

    COntinue pt2

--- day12/day12.2.py
+++ day12/day12.2.py
@@ -22,7 +22,7 @@ with open(src) as data:
     for i, l in enumerate(lines):
         l = lines[i][0]
         g = lines[i][1]
-        for j in range(4):
+        for j in range(2):
             lines[i] = [lines[i][0] + "?" + l, lines[i][1] + g]
 
 
@@ -46,77 +46,53 @@ v = 0
 
 cache = {}
 
+def pop_groups(l, gs):
+    gs = gs[:]
+    ms = re.findall(r"#+", l)
+    count = 0
+    for m in ms:
+        if len(gs) > 0 and len(m) == gs.pop(0):
+            count += 1
+        else:
+            break
+    return count
+
 def combos(line, groups, li=0, gi=0):
+    init_li = li
+    init_gi = gi
     if (li, gi) in cache.keys():
+        print("CACHE")
         return cache[(li, gi)]
 
-    if li >= len(line):
-        if validate(line, groups):
-            cache[(li,gi)] = 1
-            return 1
-        else:
-            cache[(li,gi)] = 0
-            return 0
-    
-    #print(line, groups)
-    #print(line[li:], groups[gi:])
-    springs = 0
-    just_popped = False
     li = 0
-    gi = 0
     while True:
-        if li >= len(line) or line[li] == "?":
-            #print("ENDING")
+        if li >= len(line):
+            val = 1 if validate(line, groups) else 0
+            cache[(li,gi)] = val
+            return val
+        if line[li] == "?":
             break
-        just_popped = False
-        #print(f"READING {line[li]} AT {li}")
-        if line[li] == "#":
-            springs += 1
-            if springs == groups[gi]:
-                #print(f"POPPING SPRING GROUP {gi} OF LEN {groups[gi]} AT {li}")
-                just_popped = True
-                springs = 0
-                gi+=1
-        elif line[li] == ".":
-            springs = 0
-
         li += 1
-
-    if li >= len(line) and gi != len(groups):
-        #print("FAIL")
-        cache[(li,gi)] = 0
-        return 0
-
-    if li < len(line):
-        l2 = line[:li] + "." + line[li+1:]
-        l1 = line[:li] + "#" + line[li+1:]
-        if not just_popped:
-            #print()
-            val = combos(l1, groups, li, gi) + combos(l2, groups, li, gi)
-            cache[(li,gi)] = val
-            return val
-        else:
-            #print("ALTING")
-            #print()
-            val = combos(l2, groups, li, gi)
-            cache[(li,gi)] = val
-            return val
-    else:
-        if gi == len(groups):
-            print("SUCCESS", line)
-            cache[(li,gi)] = 1
-            return 1
-        else:
-            cache[(li,gi)] = 0
-            return 0
     
+    l1 = line[:li] + "." + line[li+1:]
+    l2 = line[:li] + "#" + line[li+1:]
+    g1 = pop_groups(l1[:li+1], groups)
+    g2 = pop_groups(l2[:li+1], groups)
+    v1 = combos(l1, groups, li, g1)
+    v2 = combos(l2, groups, li, g2)
+
+    # CACHING NOT WORKING.    
+    cache[(init_li,init_gi)] = v1 + v2
+    return v1 + v2
 
 total = 0
 #print(og_lines[0], combos(*og_lines[0]))
-print(lines[0])
-print(combos(*lines[0]))
-cache = {}
-print(lines[3], combos(*lines[3]))
+#print(lines[0])
+#print(combos(*lines[3]))
+for g in [lines[0], lines[3]]:
+    cache = {}
+    print(g)
+    print(combos(*g))
 exit()
 
 
