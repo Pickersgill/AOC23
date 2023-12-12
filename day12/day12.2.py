5cc9c24d79ec8cbbb811752177697ac3603e7f50
Pickersgill <Jack.Pellen-Pickersgill.2018@live.rhul.ac.uk>
2023-12-12 18:23:41 +0000

    Partial part 2

--- day12/day12.2.py
+++ day12/day12.2.py
@@ -43,29 +43,82 @@ def validate(line, groups):
     return True
 
 v = 0
-def combos(line, groups):
-    global v
-    v += 1
-    if validate(line, groups):
-        return 1
+
+cache = {}
+
+def combos(line, groups, li=0, gi=0):
+    if (li, gi) in cache.keys():
+        return cache[(li, gi)]
+
+    if li >= len(line):
+        if validate(line, groups):
+            cache[(li,gi)] = 1
+            return 1
+        else:
+            cache[(li,gi)] = 0
+            return 0
     
-    if "?" not in line:
+    #print(line, groups)
+    #print(line[li:], groups[gi:])
+    springs = 0
+    just_popped = False
+    li = 0
+    gi = 0
+    while True:
+        if li >= len(line) or line[li] == "?":
+            #print("ENDING")
+            break
+        just_popped = False
+        #print(f"READING {line[li]} AT {li}")
+        if line[li] == "#":
+            springs += 1
+            if springs == groups[gi]:
+                #print(f"POPPING SPRING GROUP {gi} OF LEN {groups[gi]} AT {li}")
+                just_popped = True
+                springs = 0
+                gi+=1
+        elif line[li] == ".":
+            springs = 0
+
+        li += 1
+
+    if li >= len(line) and gi != len(groups):
+        #print("FAIL")
+        cache[(li,gi)] = 0
         return 0
 
-    l1 = line.replace("?", ".", 1)
-    l2 = line.replace("?", "#", 1)
-
-    c1 = combos(l1, groups)
-    c2 = combos(l2, groups)
-    return c1 + c2
+    if li < len(line):
+        l2 = line[:li] + "." + line[li+1:]
+        l1 = line[:li] + "#" + line[li+1:]
+        if not just_popped:
+            #print()
+            val = combos(l1, groups, li, gi) + combos(l2, groups, li, gi)
+            cache[(li,gi)] = val
+            return val
+        else:
+            #print("ALTING")
+            #print()
+            val = combos(l2, groups, li, gi)
+            cache[(li,gi)] = val
+            return val
+    else:
+        if gi == len(groups):
+            print("SUCCESS", line)
+            cache[(li,gi)] = 1
+            return 1
+        else:
+            cache[(li,gi)] = 0
+            return 0
+    
 
 total = 0
-print(og_lines[0], combos(*og_lines[0]))
-print(lines[0], combos(*lines[0]))
-print(og_lines[3], combos(*og_lines[3]))
+#print(og_lines[0], combos(*og_lines[0]))
+print(lines[0])
+print(combos(*lines[0]))
+cache = {}
 print(lines[3], combos(*lines[3]))
-
 exit()
+
 
 print(total)
 print(v)
